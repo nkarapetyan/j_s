fa3bf22252962b64ae4bd09a95bd338a946e87c9
Nare Karapetyan <nare.karapetyan.w@gmail.com>
2019-05-28 16:09:04 -0400

    edit on github is added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('_ext'))
 
 
 # -- Project information -----------------------------------------------------
@@ -40,6 +40,7 @@ release = u''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'edit_on_github'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -157,3 +158,6 @@ texinfo_documents = [
 
 
 # -- Extension configuration -------------------------------------------------
+
+edit_on_github_project = 'nkarapetyan/j_s'
+edit_on_github_branch = 'master'
